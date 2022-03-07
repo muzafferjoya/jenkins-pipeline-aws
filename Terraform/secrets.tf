5944648fea99f48babac35c59e41659c8fe564b8
Muzaffar Khan <muzafferjoya@gmail.com>
2022-03-07 11:50:49 +0530

    del

--- Terraform/secrets.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_secretsmanager_secret" "simple-web-app" {
-  name = "simple-web-app"
-}
-
-resource "aws_secretsmanager_secret_version" "simple-web-app" {
-  secret_id = aws_secretsmanager_secret.simple-web-app.id
-  secret_string = jsonencode(var.secrets)
-}
